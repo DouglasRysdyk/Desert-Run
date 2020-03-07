2ff26e6a3f0595e9c7de174b8185fd7c9b1199b3
Doug Rysdyk <dldrysdyk@me.com>
2020-03-07 13:25:30 -0500

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed = 10f;
+    public Text scoreHolder;
+    public float health = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreHolder.text = ("Score: " + speed);
+
+        float mH = Input.GetAxis("Horizontal");
+        float mV = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(mH, 0.0f, mV);
+
+        rb.AddForce(movement * speed);
+    }
+}
---
+++ Assets/Scripts/Treasure.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Treasure : MonoBehaviour {
+
+	public int value = 10;
+	public GameObject explosionPrefab;
+	public GameObject player;
+	private PlayerController playerCtrl;
+
+	private void Start()
+	{
+		playerCtrl = player.GetComponent<PlayerController>();
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.tag == "Player") {
+			if (GameManager.gm!=null)
+			{
+				// tell the game manager to Collect
+				GameManager.gm.Collect (value);
+			}
+
+			playerCtrl.health += 10;
+
+			// explode if specified
+			if (explosionPrefab != null) {
+				Instantiate (explosionPrefab, transform.position, Quaternion.identity);
+			}
+
+			// destroy after collection
+			Destroy (gameObject);
+		}
+	}
+}
